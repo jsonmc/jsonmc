f493c0329ab8801729b834f093224ee0c831b1f4
Alvin Ramadhan <malvinr789@gmail.com>
2018-10-22 23:36:35 +0700

    Create skyscraper.js

---
+++ movies/2018/skyscraper.js
@@ -0,0 +1,23 @@
+{
+  "name": "Skyscraper",
+  "year": 2018,
+  "runtime": 102,
+  "categories": [
+    "action",
+    "Thrller",
+    "Drama"
+  ],
+  "release-date": "2018-07-13",
+  "director": "Rawson Marshall Thurber",
+  "writer": [
+    "Rawson Marshall Thurber"
+  ],
+  "actors": [
+    "Dwayne Johnson",
+    "Neve Campbell",
+    "Chin Hann",
+    "Noah Taylor",
+    "Byron Mann"
+  ],
+  "storyline": "A security expert must infiltrate a burning skyscraper, 225 stories above ground, when his family are trapped inside by criminals."
+}
